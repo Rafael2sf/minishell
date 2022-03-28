6053d48a1ae755372c5387142e9478f0318a0c0d
rafernan <rafaelmdsbni@mail.com>
2022-03-28 22:54:17 +0100

    Added first working version of variable expansion without ~

--- src/parse/test.c
+++ src/parse/test.c
@@ -13,128 +13,138 @@
 #include "../../headers/parse.h"
 #include "../../headers/minishell.h"
 
-static int	ms_get_env_len(char *s, int *i) // not using correct indexes
-{
-	char	*tmp;
-	int		size;
-	char	c;
+static int	ms_expand_var(char *var, char *dest);
+static int	ms_expand_len(char *s, bool rmv_quote, bool *req_expand);
+static int	ms_get_env_len(char *s, int *i);
 
-	size = 0;
-	if (ft_is(s[*i], "\"\'"))
-	{
-		if (s[*i] == '\"')
-			size += 1;
-		(*i) += 1;
-		while (s[*i] && !ft_is(s[*i], "\'\" "))
-			(*i) += 1;
-		(*i) += 1;
-		return (size);
-	}
-	while (s[*i] && !ft_is(s[*i], "\'\" "))
-		(*i) += 1;
-	c = s[*i];
-	s[*i] = '\0';
-	tmp = getenv(s);
-	s[*i] = c;
-	if (!tmp) // Make strlen return 0 if null
-		return (0);
-	(*i) += 2;
-	return (strlen(tmp)); // Using forbbiden
-}
-
-static int	ms_expand_len(char *s, int rmv_quote)
+// Assuming new_s is allocated with len bytes
+int ms_expand_str(char *s, int len, bool rmv_quote, char *new_s)
 {
-	int	i;
-	int	size;
-	int	quote;
+	t_pvars	v;
 	
-	i = 0;
-	quote = 0;
-	size = 0;
-	if (s[0] == '\'')
-		quote = 1;
-	if (rmv_quote == 1 && (s[0] == '\'' || s[0] == '\"'))
+	pvars_init(&v);
+	printf("len = %d\n", len);
+	memset(new_s, '\0', len + 1); // forbbiden
+	while (v.end < len)
 	{
-		while (s[++i] == ' ')
-			;
-	}
-	while (s[i])
-	{
-		if (!quote && s[i] == '$' && (s[i + 1] != ' ' && s[i + 1]))
+		ms_parse_quotes(s[v.start], &v);
+		if (rmv_quote && ((v.start == 0  
+			|| !s[v.start + 1]) && ft_is(s[v.start], "\'\"")))
+			(v.start) += 1;
+		else if (v.quote == 0 && s[v.start] == '$'
+				&& (s[v.start + 1] != ' ' && s[v.start + 1]))
 		{
-			i += 1;
-			size += ms_get_env_len(s, &i);
+				(v.start) += ms_expand_var(&s[++v.start], &new_s[v.end]);
+				while (new_s[v.end])
+					(v.end) += 1;
 		}
 		else
-		{
-			i++;
-			size++;
-		}
+			new_s[v.end++] = s[v.start++];
 	}
-	return (size);
+	return (0);
 }
 
-int	ms_expand_var(char *var, char *dest)
+static int	ms_expand_var(char *var, char *dest)
 {
 	char c;
 	int tmp;
-	//char *envar;
 
 	tmp = 0;
 	if (*var == '\'' || *var == '\"')
 	{
 		if (*var == '\"')
 			*dest++ = '$';
-		while (var[++tmp] != '\'')
+		while (var[++tmp] && !ft_is(var[tmp], "\'\" "))
 			*dest++ = var[tmp];
 		return (tmp + 1);
 	}
-	while (var[tmp] && !ft_is(var[tmp], "\'\" "))
+	while (var[tmp] && !ft_is(var[tmp], "\'\"$ "))
 		tmp++;
 	c = var[tmp];
 	var[tmp] = '\0';
-	// dest = getenv(var);
-	// while (envar && *envar)
-	// 	*dest++ = *envar++;
-	strcpy(dest, getenv(var)); // Forbbiden
+	if (getenv(var)) // Protect agaisnt NULL
+		strcpy(dest, getenv(var)); // Forbbiden
 	var[tmp] = c;
 	return (tmp);
 }
 
-// Assuming new_s is allocated with len bytes
-int ms_expand_str(char **s, int len, bool rmv_quote, char *new_s)
+static int	ms_expand_len(char *s, bool rmv_quote, bool *req_expand) // v.end is used as size
 {
 	t_pvars	v;
 	
 	pvars_init(&v);
-	// new_s = malloc(sizeof(char) * len + 1);
-	// if (!new_s)
-	// 	return (-1);
-	memset(new_s, '\0', len + 1);
-	while (v.end < len)
+	if (v.start == 0 && rmv_quote && ft_is(s[v.start], "\'\""))
 	{
-		ms_parse_quotes((*s)[v.start], &v);
-		if (rmv_quote && ((v.start == 0  
-			|| !(*s)[v.start + 1]) && ft_is((*s)[v.start], "\'\"")))
-			(v.start) += 1;
-		else if (v.quote == 0 && (*s)[v.start] == '$'
-				&& ((*s)[v.start + 1] != ' ' && (*s)[v.start + 1]))
+		(v.end) -= 2;
+		(*req_expand) = true;
+	}
+	while (s[v.start])
+	{
+		ms_parse_quotes(s[v.start], &v);
+		if (v.quote == 0 && s[v.start] == '$' && (s[v.start + 1] != ' ' && s[v.start + 1]))
 		{
-				(v.start) += ms_expand_var(&(*s)[++v.start], &new_s[v.end]);
-				while (new_s[v.end])
-					(v.end) += 1;
+			(v.end) += ms_get_env_len(s, &v.start);
+			(*req_expand) = true;
 		}
 		else
-			new_s[v.end++] = (*s)[v.start++];
+		{
+			(v.start) += 1;
+			(v.end) += 1;
+		}
 	}
-	return (0);
+	return (v.end);
 }
+
+static int	ms_get_env_len(char *s, int *i)
+{
+	char	*tmp;
+	int		size;
+	char	c;
+
+	size = 0;
+	(*i) += 1;
+	tmp = &s[*i];
+	if (ft_is(s[*i], "\"\'"))
+	{
+		if (s[*i] == '\"')
+			size += 1;
+		while (s[++(*i)] && !ft_is(s[*i], "\'\" "))
+			size += 1;
+		(*i) += 1;
+		return (size);
+	}
+	while (s[*i] && !ft_is(s[*i], "\'\"$ "))
+		(*i) += 1;
+	c = s[*i];
+	s[*i] = '\0';
+	tmp = getenv(tmp);
+	s[*i] = c;
+	if (!tmp) // strlen handle null
+		return (0);
+	return (strlen(tmp)); // Using forbbiden
+}
+
+# define LEN 8
+# define RQ	1
 
 int		main(void)
 {
-	char	buf[50];
-	memcpy(buf, "$USER", 5);
-	buf[5] = 0;
-	printf("%d\n", ms_expand_len(buf, 1));
+	char	str[200];
+	char	buf[200];
+	int		len;
+	bool	req;
+
+	memcpy(str, "\"'$a$b'\"", LEN);
+	str[LEN] = 0;
+	printf("str = %s\n", str);
+	len = ms_expand_len(str, RQ, &req);
+	printf("(req: %d)\n", req);
+	if (req)
+	{
+		ms_expand_str(str, len, RQ, buf);
+		printf("buf = %s\n", buf);
+	}
+	else
+		printf("buf = %s\n", str);
 	return (0);
 }

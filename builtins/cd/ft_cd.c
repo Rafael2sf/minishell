1821a8d37c200f015404956b72f86c204d2f6a10
Dario Almeida barroca <daalmeid@c1r20s4.42lisboa.com>
2022-03-31 11:25:13 +0100

    changed echo and env and added pwd and cd

---
+++ builtins/cd/ft_cd.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cd.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daalmeid <daalmeid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/21 15:50:08 by daalmeid          #+#    #+#             */
+/*   Updated: 2022/03/30 11:32:53 by daalmeid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/minishell.h"
+#include "../../headers/libft.h"
+#include "../export/ft_export.h"
+
+static void	error_message(char *input)
+{
+	ft_putstr_fd("cd: ", 2);
+	ft_putstr_fd(input, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putendl_fd(strerror(errno), 2);
+}
+
+static int	null_input_check(char **input, char **env)
+{
+	int		i;
+
+	if (!*input || !(*input)[0])
+	{
+		i = 0;
+		while (env[i] != NULL)
+		{
+			if (ft_strncmp("HOME=", env[i], 5) == 0)
+			{
+				*input = &env[i][5];
+				break ;
+			}
+			i++;
+		}
+		if (env[i] == NULL)
+		{
+			ft_putendl_fd("cd: HOME not set", 2);
+			return (0);
+		}
+	}
+	return (1);
+}
+
+char	**ft_cd(char **input, int fd, char **env)		//fd?????
+{
+	char	buf[1024];
+	char	*pwd_change;
+
+	if (null_input_check(&input[1], env) == 0)
+		return (env);
+	getcwd(buf, 1024);
+	if (chdir(input[1]) == -1)
+	{
+		error_message(input[1]);
+		return (env);
+	}
+	pwd_change = ft_strjoin("OLDPWD=", buf);
+	env = ft_export(pwd_change, fd, env);
+	free(pwd_change);
+	getcwd(buf, 1024);
+	pwd_change = ft_strjoin("PWD=", buf);
+	env = ft_export(pwd_change, fd, env);
+	free(pwd_change);
+	return (env);
+}
+
+int	main(int ac, char **av, char **env)
+{
+	char	**env_cpy;
+	(void) ac;
+	(void) av;
+
+	env_cpy = creat_copy(env);
+	ft_cd(NULL, 1, env_cpy);
+	ptr_ptr_free((void **) env_cpy);
+	return (0);
+}
--- builtins/env/ft_env.c
+++ builtins/env/ft_env.c
@@ -6,71 +6,26 @@
 /*   By: daalmeid <daalmeid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 10:18:36 by daalmeid          #+#    #+#             */
-/*   Updated: 2022/03/22 12:08:26 by daalmeid         ###   ########.fr       */
+/*   Updated: 2022/03/30 15:00:55 by daalmeid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 #include "../../headers/libft.h"
 
-void	full_env_list(char *output, char **env, int *total)
+int	ft_env(char **input, char **env, int fd)
 {
 	int		i;
-	int		j;
 
+	(void) input;
 	i = 0;
-	*total = 0;
 	while (env[i] != NULL)
-	{
-		j = 0;
-		while (env[i][j] != '\0')
-		{
-			output[(*total)++] = env[i][j++];
-		}
-		output[(*total)++] = '\n';
-		i++;
-	}
-	output[(*total)] = '\0';
-}
-
-int	ft_env(char *input, int fd, char **env)
-{
-	int	i;
-	int	j;
-	int total;
-	char	*output;
-
-	if (!env)
-		return (-1);
-	i = 0;
-	total = 0;
-	while (env[i] != NULL)
-	{
-		j = 0;
-		while (env[i][j] != '\0')
-			j++;
-		total += j;
-		i++;
-	}
-	output = malloc(sizeof(char) * (total + i + 1));
-	if (!output)
-		return (-1);
-	full_env_list(output, env, &total);
-	write(fd, output, total + 1);
-	free(output);
+		ft_putendl_fd(env[i++], fd);
 	return (1);
 }
 
 /*int	main(int ac, char **av, char **env)
 {
-	int	i = 0;
-
-	while (env[i] != NULL)
-	{
-		printf("%s\n", env[i++]);
-	}
-	ft_env(NULL, 1, env);
+	ft_env(env,1);
 	return (0);
 }*/
-
-
